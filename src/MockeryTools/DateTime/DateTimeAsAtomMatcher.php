48f4eb8877aa4a5ccff859278ff0ba318028877d
Dominik Voda <d.voda94@gmail.com>
2020-03-25 18:14:51 +0100

    + date time as atom matcher (#9)

---
+++ src/MockeryTools/DateTime/DateTimeAsAtomMatcher.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\MockeryTools\DateTime;
+
+use DateTime;
+use DateTimeImmutable;
+use Mockery\Matcher\MatcherAbstract;
+use function assert;
+
+final class DateTimeAsAtomMatcher extends MatcherAbstract
+{
+    /**
+     * @var string
+     */
+    private $expectedDateTimeAsAtom;
+
+
+    public function __construct(string $expectedDateTimeAsAtom)
+    {
+        parent::__construct();
+        $this->expectedDateTimeAsAtom = $expectedDateTimeAsAtom;
+    }
+
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     *
+     * @param mixed $actual
+     */
+    public function match(&$actual): bool
+    {
+        assert($actual instanceof DateTimeImmutable);
+
+        return $actual->format(DateTime::ATOM) === $this->expectedDateTimeAsAtom;
+    }
+
+
+    public function __toString(): string
+    {
+        return '<DateTimeMatch[' . $this->expectedDateTimeAsAtom . ']>';
+    }
+}
