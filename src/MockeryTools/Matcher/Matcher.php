9aec701435e179c71d7629c5c116537643c97c01
Michal Brauner <michal.brauner@niceincontact.com>
2020-04-06 12:52:45 +0200

    add uri matcher factory

--- src/MockeryTools/Matcher/Matcher.php
+++ src/MockeryTools/Matcher/Matcher.php
@@ -4,6 +4,8 @@ namespace BrandEmbassy\MockeryTools\Matcher;
 
 use BrandEmbassy\MockeryTools\DateTime\DateTimeAsAtomMatcher;
 use BrandEmbassy\MockeryTools\String\StringStartsWithMatcher;
+use BrandEmbassy\MockeryTools\Uri\UriMatcher;
+use Psr\Http\Message\UriInterface;
 
 final class Matcher
 {
@@ -16,5 +18,11 @@ final class Matcher
     public static function dateTimeAsAtom(string $expectedDateTimeInAtom): DateTimeAsAtomMatcher
     {
         return new DateTimeAsAtomMatcher($expectedDateTimeInAtom);
+    }
+
+
+    public static function uri(string $expectedUri): UriMatcher
+    {
+        return new UriMatcher($expectedUri);
     }
 }
---
+++ src/MockeryTools/Uri/UriMatcher.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\MockeryTools\Uri;
+
+use Mockery\Matcher\MatcherAbstract;
+
+/**
+ * @property string $_expected
+ */
+final class UriMatcher extends MatcherAbstract
+{
+    public function __construct(string $expected)
+    {
+        parent::__construct($expected);
+    }
+
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     *
+     * @param mixed $actual
+     */
+    public function match(&$actual): bool
+    {
+        return (string)$actual === $this->_expected;
+    }
+
+
+    public function __toString(): string
+    {
+        return '<Uri:' . $this->_expected . '>';
+    }
+}
