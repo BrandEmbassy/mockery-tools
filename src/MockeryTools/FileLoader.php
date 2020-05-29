1031d1ee30d34e31446857fd8b8cdcaf3d3b1eb4
Michal Tichý <michal.tichy.84@gmail.com>
2020-05-29 13:25:48 +0200

    Adding tool for loading JSON from file with placeholders replacement

--- src/MockeryTools/FileLoader.php
+++ src/MockeryTools/FileLoader.php
@@ -2,7 +2,9 @@
 
 namespace BrandEmbassy\MockeryTools;
 
+use BrandEmbassy\MockeryTools\Json\JsonValuesReplacer;
 use LogicException;
+use Nette\IOException;
 use Nette\Utils\FileSystem;
 use Nette\Utils\Json;
 use Nette\Utils\JsonException;
@@ -11,19 +13,63 @@ final class FileLoader
 {
     /**
      * @return mixed[]
+     *
+     * @throws LogicException
      */
-    public static function loadArrayFromJsonFile(string $filePath): array
+    public static function loadArrayFromJsonFile(string $jsonFilePath): array
     {
-        try {
-            return Json::decode(self::loadAsString($filePath), Json::FORCE_ARRAY);
-        } catch (JsonException $exception) {
-            throw new LogicException('File ' . $filePath . ' is not valid JSON');
-        }
+        return self::loadArrayFromJsonFileAndReplace($jsonFilePath, []);
     }
 
 
+    /**
+     * @param mixed[] $valuesToReplace
+     *
+     * @return mixed[]
+     *
+     * @throws LogicException
+     */
+    public static function loadArrayFromJsonFileAndReplace(string $jsonFilePath, array $valuesToReplace): array
+    {
+        $fileContents = self::loadJsonStringFromJsonFileAndReplace($jsonFilePath, $valuesToReplace);
+
+        return self::decodeJson($jsonFilePath, $fileContents);
+    }
+
+
+    /**
+     * @param mixed[] $valuesToReplace
+     *
+     * @throws LogicException
+     */
+    public static function loadJsonStringFromJsonFileAndReplace(string $jsonFilePath, array $valuesToReplace): string
+    {
+        $fileContents = self::loadAsString($jsonFilePath);
+
+        return JsonValuesReplacer::replace($valuesToReplace, $fileContents);
+    }
+
+
+    /**
+     * @throws IOException
+     */
     public static function loadAsString(string $filePath): string
     {
         return FileSystem::read($filePath);
+    }
+
+
+    /**
+     * @return mixed[]
+     *
+     * @throws LogicException
+     */
+    private static function decodeJson(string $jsonFilePath, string $fileContents): array
+    {
+        try {
+            return Json::decode($fileContents, Json::FORCE_ARRAY);
+        } catch (JsonException $exception) {
+            throw new LogicException('File ' . $jsonFilePath . ' is not JSON: ' . $exception->getMessage());
+        }
     }
 }
