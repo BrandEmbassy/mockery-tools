6a87fe77bfc2b746a7ddf33ea1ea0bad4adedfd4
Dominik Voda <d.voda94@gmail.com>
2020-08-12 17:56:41 +0200

    + uuid & response assertions (#18)
    * + uuid assertions
    + response assertions
    
    * + annotations

---
+++ src/MockeryTools/Http/ResponseAssertions.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\MockeryTools\Http;
+
+use Nette\Utils\Json;
+use PHPUnit\Framework\Assert;
+use Psr\Http\Message\ResponseInterface;
+
+final class ResponseAssertions
+{
+    public static function assertJsonResponseEqualsJsonFile(string $jsonFilePath, ResponseInterface $response): void
+    {
+        Assert::assertJsonStringEqualsJsonFile($jsonFilePath, self::getResponseBody($response));
+    }
+
+
+    public static function assertJsonResponseEqualsJsonString(string $expectedJson, ResponseInterface $response): void
+    {
+        Assert::assertJsonStringEqualsJsonString($expectedJson, self::getResponseBody($response));
+    }
+
+
+    /**
+     * @param mixed[] $expectedArray
+     */
+    public static function assertJsonResponseEqualsArray(array $expectedArray, ResponseInterface $response): void
+    {
+        $expectedJson = Json::encode($expectedArray);
+
+        Assert::assertJsonStringEqualsJsonString($expectedJson, self::getResponseBody($response));
+    }
+
+
+    /**
+     * @param array<string, string> $expectedHeaders
+     */
+    public static function assertResponseHeaders(array $expectedHeaders, ResponseInterface $response): void
+    {
+        foreach ($expectedHeaders as $headerName => $headerValue) {
+            self::assertResponseHeader($headerValue, $headerName, $response);
+        }
+    }
+
+
+    public static function assertResponseHeader(
+        string $expectedHeaderValue,
+        string $headerName,
+        ResponseInterface $response
+    ): void {
+        Assert::assertSame($expectedHeaderValue, $response->getHeaderLine($headerName));
+    }
+
+
+    public static function assertResponseStatusCode(int $expectedStatusCode, ResponseInterface $response): void
+    {
+        Assert::assertSame($expectedStatusCode, $response->getStatusCode());
+    }
+
+
+    private static function getResponseBody(ResponseInterface $response): string
+    {
+        return (string)$response->getBody();
+    }
+}
---
+++ src/MockeryTools/String/UuidAssertions.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\MockeryTools\String;
+
+use PHPUnit\Framework\Assert;
+use Ramsey\Uuid\UuidInterface;
+
+final class UuidAssertions
+{
+    public static function assertStringUuidEqualsUuid(string $expectedUuidString, UuidInterface $uuid): void
+    {
+        Assert::assertSame($expectedUuidString, $uuid->toString());
+    }
+
+
+    public static function assertUuidEqualsUuid(UuidInterface $expectedUuid, UuidInterface $uuid): void
+    {
+        Assert::assertSame($expectedUuid->toString(), $uuid->toString());
+    }
+}
