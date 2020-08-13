75d681816edb4d5c829c2b07a5b2b75452ba11bf
Dominik Voda <d.voda94@gmail.com>
2020-08-13 14:18:56 +0200

    + ResponseBodyParser (#20)

---
+++ src/MockeryTools/Http/ResponseBodyParser.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\MockeryTools\Http;
+
+use Nette\Utils\Json;
+use Psr\Http\Message\ResponseInterface;
+
+final class ResponseBodyParser
+{
+    public static function parseAsString(ResponseInterface $response): string
+    {
+        return (string)$response->getBody();
+    }
+
+
+    /**
+     * @return mixed[]
+     */
+    public static function parseAsArray(ResponseInterface $response): array
+    {
+        return Json::decode(self::parseAsString($response), Json::FORCE_ARRAY);
+    }
+}
