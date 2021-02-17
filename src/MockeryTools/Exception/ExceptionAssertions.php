b7ad26af62b76de5d909dbe17de4fd21d73c20cc
Michal Tichý <michal.tichy.84@gmail.com>
2021-02-17 21:09:48 +0100

    ExceptionAssertions fix end extension of expectations

--- src/MockeryTools/Exception/ExceptionAssertions.php
+++ src/MockeryTools/Exception/ExceptionAssertions.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use Throwable;
 use function get_class;
 use function sprintf;
+use function strpos;
 
 final class ExceptionAssertions
 {
@@ -20,18 +21,40 @@ final class ExceptionAssertions
     public static function assertExceptionCallback(
         string $expectedExceptionClassName,
         Closure $expectation,
-        Closure $callback
+        Closure $callback,
+        ?string $expectedExceptionMessage = null,
+        ?int $expectedExceptionCode = null
     ): void {
         try {
             $callback();
         } catch (Throwable $exception) {
-            if ($exception instanceof $expectedExceptionClassName) {
+            if (!($exception instanceof $expectedExceptionClassName)) {
                 TestCase::fail(
                     sprintf(
                         'Exception "%s" expected, but given "%s" with message: "%s".',
                         $expectedExceptionClassName,
                         get_class($exception),
                         $exception->getMessage()
+                    )
+                );
+            }
+            if ($expectedExceptionMessage !== null
+                && strpos($exception->getMessage(), $expectedExceptionMessage) !== false
+            ) {
+                TestCase::fail(
+                    sprintf(
+                        'Exception message "%s" does not contain expected "%s"',
+                        $exception->getMessage(),
+                        $expectedExceptionMessage
+                    )
+                );
+            }
+            if ($expectedExceptionCode !== null && $exception->getCode() !== $expectedExceptionCode) {
+                TestCase::fail(
+                    sprintf(
+                        'Exception code %d is different from expected code %d.',
+                        $exception->getCode(),
+                        $expectedExceptionCode
                     )
                 );
             }
