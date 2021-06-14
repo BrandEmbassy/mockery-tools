684aa60a0a5dde820bb247c0bdd4fd50471367c5
Milan Zelenka <milan@milanzelenka.com>
2021-06-14 18:06:25 +0200

    Replace values when asserting snapshot

--- src/MockeryTools/Snapshot/SnapshotAssertions.php
+++ src/MockeryTools/Snapshot/SnapshotAssertions.php
@@ -5,18 +5,47 @@ namespace BrandEmbassy\MockeryTools\Snapshot;
 use BrandEmbassy\MockeryTools\FileLoader;
 use PHPUnit\Framework\Assert;
 use Psr\Http\Message\ResponseInterface;
+use function array_keys;
+use function array_map;
+use function array_values;
+use function sprintf;
 use function str_replace;
 
 final class SnapshotAssertions
 {
     public static function assertSnapshot(string $snapshotFile, string $testedOutput): void
     {
-        $snapshot = FileLoader::loadAsString($snapshotFile);
-
-        $normalizedSnapshot = self::normalizeHtmlInput($snapshot);
+        $normalizedSnapshot = self::loadNormalizedSnapshot($snapshotFile);
         $testedOutput = self::normalizeHtmlInput($testedOutput);
 
         Assert::assertSame($normalizedSnapshot, $testedOutput);
+    }
+
+
+    /**
+     * @param array<string, string> $valuesToReplace
+     */
+    public static function assertSnapshotAndReplace(
+        string $snapshotFile,
+        string $testedOutput,
+        array $valuesToReplace
+    ): void {
+        $snapshot = self::loadNormalizedSnapshot($snapshotFile);
+        $keys = array_map(
+            static function (string $key): string {
+                return sprintf('{{%s}}', $key);
+            },
+            array_keys($valuesToReplace)
+        );
+        $snapshotWithReplacedValues = str_replace(
+            $keys,
+            array_values($valuesToReplace),
+            $snapshot
+        );
+
+        $testedOutput = self::normalizeHtmlInput($testedOutput);
+
+        Assert::assertSame($snapshotWithReplacedValues, $testedOutput);
     }
 
 
@@ -29,5 +58,13 @@ final class SnapshotAssertions
     private static function normalizeHtmlInput(string $input): string
     {
         return str_replace(['  ', "\n", "\r", "\t"], '', $input);
+    }
+
+
+    private static function loadNormalizedSnapshot(string $snapshotFile): string
+    {
+        $snapshot = FileLoader::loadAsString($snapshotFile);
+
+        return self::normalizeHtmlInput($snapshot);
     }
 }
