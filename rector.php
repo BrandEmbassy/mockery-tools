5e268420549b334deb26754238977140051d9dca
Dominik Vašíček <vasido@seznam.cz>
2025-08-11 15:32:39 +0200

    Update minimum PHP, add missing dependencies, update coding standard and add rector to the ci and commands

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+
+$rectorConfigBuilder = RectorConfig::configure();
+$defaultRectorConfigurationSetup = require 'vendor/brandembassy/coding-standard/default-rector.php';
+
+$defaultSkipList = $defaultRectorConfigurationSetup($rectorConfigBuilder);
+
+$rectorConfigBuilder
+    ->withPHPStanConfigs([__DIR__ . '/phpstan.neon'])
+    ->withCache('./temp/rector', FileCacheStorage::class)
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withSkip($defaultSkipList);
+
+return $rectorConfigBuilder;
