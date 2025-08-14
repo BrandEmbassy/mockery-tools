0b6d6f132845442b592ce6740fda12bcc7aec7fe
Dominik Vašíček <vasido@seznam.cz>
2025-08-14 15:16:03 +0200

    Update dependencies and coding standards

--- ecs.php
+++ ecs.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types = 1);
 
-use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 $defaultEcsConfigurationSetup = require 'vendor/brandembassy/coding-standard/default-ecs.php';
@@ -14,22 +13,6 @@ return static function (ECSConfig $ecsConfig) use ($defaultEcsConfigurationSetup
     ]);
 
     $skipList = [];
-
-    if (PHP_VERSION_ID < 81000) {
-        $skipList[ParameterTypeHintSniff::class . '.UselessSuppress'] = [
-            'src/MockeryTools/Arrays/ArraySubsetConstraint.php',
-            'src/MockeryTools/Arrays/StrictArrayMatcher.php',
-            'src/MockeryTools/DateTime/DateTimeAsAtomMatcher.php',
-            'src/MockeryTools/DateTime/DateTimeAsTimestampMatcher.php',
-            'src/MockeryTools/DateTime/DateTimeZoneNameMatcher.php',
-            'src/MockeryTools/Enum/EnumValueMatcher.php',
-            'src/MockeryTools/Http/HttpRequestMatcher.php',
-            'src/MockeryTools/Http/HttpRequestOptionsMatcher.php',
-            'src/MockeryTools/String/StringStartsWithMatcher.php',
-            'src/MockeryTools/String/UuidMatcher.php',
-            'src/MockeryTools/Uri/UriMatcher.php',
-        ];
-    }
 
     $ecsConfig->skip(array_merge($defaultSkipList, $skipList));
 };
